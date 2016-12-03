a09022d786dac310e324d0d1d8c3159584420e9a
techy101 <mvaras101@gmail.com>
2016-12-03 16:47:23 -0600

    Completed initial coding of emg.c and emg.h
    Initial setup of emg.c and emg.h has been completed. Code merged in from
    multiple previous programs. It has not been compiled in MicroC yet and
    likely contains errors.

--- Software/Final Code Working Directory/Complete Project/emg.c
+++ Software/Final Code Working Directory/Complete Project/emg.c
@@ -1,9 +1,11 @@
+#include <emg.h>
+
 /*
 Prosthetic Hand Senior Design Project
-9/25/2016
-Rachel Mertz
+12/3/2016
 Matthew Varas
 Donnell Jones
+Rachel Mertz 
 
  * File name:
     emg.c
@@ -12,14 +14,12 @@ Donnell Jones
     C file for all EMG related functions and interrupts  
 */
 
-#include <emg.h>
-
 
 
 
 /****************  Electromyography Initialization  *****************
 /																	/
-/	Function Name: initialize_emg()									/
+/	Function Name: init_emg()										/
 /	Return Type: None												/
 /																	/
 /	Description: 													/
@@ -31,31 +31,53 @@ Donnell Jones
 /																	/
 /	Postconditions: 												/
 /		All EMG and AWD configurations are complete 				/
+/		EMG Timer interrupt (UIE) is left disabled because it is    /
+/		dealt with in the AWD interrupt handler.					/
 /																	/
 /*******************************************************************/
-void initialize_emg() {														// Initialization of remaining Electromyography system
-	ADC_Set_Input_Channel(_ADC_CHANNEL_3);                    				// Choose ADC channel
-	GPIO_Config(&MASK_AWD_PORT_BASE, MASK_AWD_INPUT_PIN, (_GPIO_CFG_MODE_ANALOG | _GPIO_CFG_PULL_NO));		//Set port and pin for EMG analog input (Pulldown = NO)
-	//ADC?_Init();                                               			// Call MikroC built-in ADC Channel ? initialization function 
-    //ADC?_LTR = AWD_INITIAL_LOW_THRESH;     								// Set AWD Guarded zone low threshold value to initial state (defined in emg.h)
-    //ADC?_HTR = AWD_INITIAL_HIGH_THRESH;  									// Set AWD Guarded zone high threshold value to initial state (defined in emg.h)
-    //ADC?_CR1bits.AWDIE = 1;   											// Enable analog interrupt
-    //ADC?_CR1bits.AWDEN = 1;    											// Enable Analog watchdog on regular channels
-	
-	//Initialize AWD Interrupt   ***Unsure of code here, waiting for Donnell***
-     /*
-     ADC1_CR1bits.AWDSGL = 0;
-     ADC1_CR1bits.JAWDEN = 0;
-     ADC1_CR1bits.SCAN = 1;
-     ADC1_CR1bits.OVRIE = 1;   //Enable overrun interrupt
-	 NVIC_IntEnable(IVT_INT_WWDG);
-    */	
+void init_emg() {					
+	 /* ------------ ADC Initialization ------------ */
+     ADC_Set_Input_Channel(_ADC_CHANNEL_1);     				// Set ADC channel 1 active for EMG input signal 
+     ADC1_Init();                                               // Initialize ADC1
+
+     /* ------------ Analog Watchdog Initialization ------------ */
+     ADC1_LTR = low_level;        // Set AWD guard window initial lower threshold
+     ADC1_HTR = high_level;       // Set AWD guard window initial upper threshold
+     ADC1_CR2bits.CONT = 1;       // Enable ADC1 continuous conversion mode
+     ADC1_SQR3bits.SQ1 = 1;       // Set first channel in continuous coversion sequence to channel 3
+     ADC1_CR1bits.AWDSGL = 1;     // Enable single channel monitoring mode for AWD
+     ADC1_CR1 |= 1;               // Set channel 3 as the single monitored AWD channel
+     ADC1_CR1bits.AWDEN = 1;      // Enable Analog watchdog on regular channels
+     ADC1_CR2bits.SWSTART = 1;    // Start ADC1 Conversions
+     ADC1_CR1bits.AWDIE = 1;      // Enable analog watchdog interrupt
+     NVIC_IntEnable(IVT_INT_ADC); // Enable global ADC interrupt
+	 
+	 
+	 /* ------------ EMG Timer (10) Initialization ------------ */
+     RCC_APB2ENR.TIM10EN = 1;       		// Enable clock gating for timer 10
+     TIM10_CR1.CEN = 0;              		// Disable timer/counter
+     TIM10_PSC = EMG_TIMER_PSC;         	// Set timer 10 prescaler
+     TIM10_ARR = EMG_TIMER_RELOAD;      	// Set timer 10 overflow value 
+     NVIC_IntEnable(IVT_INT_TIM1_UP_TIM10); // Enable timer 10 interrupt
+     TIM10_CR1.CEN = 1;              		// Enable timer/counter
+	 
+	 
+	 /* ------------ EMG Override Button (EXTI) Initialization ------------ */   //B3
+	 GPIO_Digital_input(&GPIOB_BASE, _GPIO_PINMASK_3);		// Enable digital input on B3 
+	 SYSCFGEN_bit = 1;					//Enable system config controller clock 
+	 SYSCFG_EXTICR1bits.EXTI3 = 1;		// Map external interrupt 3 to port B 
+	 EXTI_RTSRbits.TR3 = 1;				// Set interrupt on rising edge 
+	 EXTI_IMRbits.MR3 = 1;				// Unmask bit 3 to enable external interrupt on line 3
+	 NVIC_IntEnable(IVT_INT_EXTI3);     //Enable external interrupt B3 for EMG override button 
 }
 
 
-/*********************  AWD Interrupt Handler  **********************
+
+
+
+/***************  Analog Watchdog Interrupt Handler  ****************
 /																	/
-/	Function Name: ADC_AWD()										/
+/	Function Name: AWD_ISR()										/
 /	Interrupt Handler												/
 /																	/
 /	Description: 													/
@@ -66,80 +88,102 @@ void initialize_emg() {														// Initialization of remaining Electromyogr
 /		Signal monitored by AWD moved outside the guarded zone		/
 /																	/
 /	Postconditions: 												/
-/		AWD is disabled, range is reset, main execution timer 	    /
-/		interrupt is executed, and AWD is finally re-enabled. For	/
-/		now this ISR handles all code, but this may be transfered   /
-/		to a state in the main code and setting a flag. However		/
-/		this ISR can override all other code, so if it executes 	/
-/		quickly enough the code will be left inside the ISR, unlike	/
-/		all other ISR's 											/
+/		Timer is armed if AWD is in initial state 					/
+/		AWD is reset and timer disarmed is AWD is in armed state 	/ 
 /																	/
 /*******************************************************************/
-void ADC_AWD() iv IVT_INT_WWDG ics ICS_AUTO {								//Declaring ISR for analog watchdog (***Does this need to be named this, or can we be more descriptive***)
-																			//Disable AWD interrupt 
-																			//Disable AWD (forget name of this, but it's the bit set in the ADC (AWD?) register)
-																			//Clear AWD interrupt bit 
-																			//Clear AWD bit  (forget name of this one)
-																			//Execute appropriate AWD range change (call change window function)
-																			//Set state flag for initial or armed state (check range) 
-																			//Re-Enable AWD Interrupt 
-																			//Re-Enable AWD bit 	
+void AWD_ISR() iv IVT_INT_ADC ics ICS_AUTO {
+      ADC1_CR1bits.AWDIE = 0;       // Disable ADC1 interrupt
+      ADC1_SRbits.AWD = 0;          // Clear AWD event flag 
+      
+	  // Analog Watchdog is in initial state (User just flexed muscle)
+	  if(ADC1_HTR <= 1000) {
+            TIM10_SR.UIF = 0;       // Clear timer 10 interrupt flag
+            TIM10_CNT = 0x00;       // Reset timer value to 0
+            ADC1_HTR = high_level2; // Set high threshold to MAX (Armed high level)
+            ADC1_LTR = low_level2;  // Set high threshold to initial high level (Armed low level)
+            TIM10_DIER.UIE = 1;     // Enable emg_timer (Timer 10) interrupts 
+	  }	
+		
+	  // Analog Watchdog is in armed state (User release muscle flex prior to emg_timer timout (3s))
+      else {
+            TIM10_DIER.UIE = 0;     // Disable emg_timer (Timer 10) interrupts 
+            ADC1_HTR = high_level;  // Set high threshold back to initial value 
+            ADC1_LTR = low_level;   // Set low threshold back to initial value 
+            }
+			
+      ADC1_CR1bits.AWDIE = 1;       //Enable ADC1 interrupt 
 }
 
 
 
-/****************  Initial AWD Guarded Zone Setup  ******************
+
+/*********  EMG Override Button (EXTI) Interrupt Handler  ***********
 /																	/
-/	Function Name: set_initial_awd_window()							/
-/	Return Type: None												/
+/	Function Name: emg_override_ISR()								/
+/	Interrupt Handler												/
 /																	/
 /	Description: 													/
-/		This function configures the analog watchdog guarded zone   /
-/		to its standby state. This is the state where it's looking  /
-/		for a low-to-high transition (initial flexing of muscle)	/
+/		This ISR overrides EMG inputs and activates the system. 	/
+/		It is used for testing and debug only. 						/
+/		Override button is a hardware button on the PCB				/
 /																	/
 /	Preconditions: 													/
-/		AWD and EMG have been initialized							/
+/		EMG override button external interrupt has been configured  /
+/		EMG override button has been pressed						/
 /																	/
 /	Postconditions: 												/
-/		AWD Guarded zone window will be set to initial(standby) 	/
-/		state. 														/
+/		System is immediatly activated 								/
 /																	/
 /*******************************************************************/
-void set_initial_awd_window() {												//Sets the AWD window to the initial state (waiting for low->high transition)
-    //ADC?_LTR = AWD_INITIAL_LOW_THRESH;     								// Set AWD Guarded zone low threshold value to initial state (defined in emg.h)
-    //ADC?_HTR = AWD_INITIAL_HIGH_THRESH;  									// Set AWD Guarded zone high threshold value to initial state (defined in emg.h)
+void emg_override_ISR() iv IVT_INT_EXTI3 ICS_AUTO {
+	EXTI_PR.B3 = 1;				// Clear interrupt event flag 
+	system_go = 1;				// Activate hand 
+	go_status = ~go_status;		// Toggle status of system (Relies on variable initialized to 0, probably better way to do this)
 }
 
 
 
-/*****************  Armed AWD Guarded Zone Setup  *******************
+
+
+
+/******************  EMG Timer Interrupt Handler  *******************
 /																	/
-/	Function Name: set_armed_awd_window()							/
-/	Return Type: None												/
+/	Function Name: emg_timer_ISR()									/
+/	Interrupt Handler 												/
 /																	/
 /	Description: 													/
-/		This function configures the analog watchdog guarded zone   /
-/		to its armed state. This is the state where it's looking	/
-/		for a high-to-low transition (releasing of muscle flex)		/
+/		This interrupt handler activates only if the EMG signal has /
+/		crossed out of the inital AWD guard zone AND three seconds  /
+/		have passed without the AWD signal falling out of the 		/
+/		armed guard zone. It resets the AWD thresholds to their 	/
+/		initial states and disables its own overflow interrupt		/
 /																	/
 /	Preconditions: 													/
-/		AWD and EMG have been initialized							/
+/		Three seconds have passed since timer was activated 		/
 /																	/
 /	Postconditions: 												/
-/		AWD Guarded zone window will be set to armed state		 	/
+/		AWD low and high thresholds are set to their initial states	/
+/		Emg timer (Timer 10) overflow interrupts are disabled 		/
 /																	/
 /*******************************************************************/
-void set_armed_awd_window() {												//Sets the AWD window to the armed state (waiting for the high->low transition) 
-    //ADC?_LTR = AWD_ARMED_LOW_THRESH;     									// Set AWD Guarded zone low threshold value to initial state (defined in emg.h)
-    //ADC?_HTR = AWD_ARMED_HIGH_THRESH;  									// Set AWD Guarded zone high threshold value to initial state (defined in emg.h)
+void emg_timer_ISR() iv IVT_INT_TIM1_UP_TIM10 { 	
+     TIM10_SR.UIF = 0;                     	// Clear timer 10 interrupt flag
+     ADC1_HTR = high_level;               	// Reset AWD high threshold to initial state 
+     ADC1_LTR = low_level;                	// Reset AWD low threshold to initial state 
+     TIM10_DIER.UIE = 0;                   	// Disable timer 10 interrupt
+	 go_status = ~go_status;				// Toggle status of system (Relies on variable initialized to 0, probably better way to do this)
+     system_go = 1;							// Go flag to start the whole system in main.c 
 }
+
+
+
 
 
 
 /******************  Get Current EMG Signal Value  ******************
 /																	/
-/	Function Name: get_current_emg_value()							/
+/	Function Name: read_emg()										/
 //	Return Type: unsigned long										/
 /																	/
 /	Description: 													/
@@ -153,9 +197,24 @@ void set_armed_awd_window() {												//Sets the AWD window to the armed stat
 /		Current value of EMG signal is returned 					/
 /																	/
 /*******************************************************************/
-unsigned long get_current_emg_value() {										//Samples the EMG signal and returns the current value 
-	unsigned long currentADCValue;											//Local variable to store and return read value from EMG ADC 
-	currentADCValue = ADC?_Get_Sample(CHANNEL_ADC_EMG_SIGNAL);				//Sample EMG signal 
+unsigned long read_emg() {								
+	unsigned long currentADCValue;									//Local variable to store and return read value from EMG ADC 
+	
+	currentADCValue = ADC?_Get_Sample(CHANNEL_ADC_EMG);				//Sample EMG signal 
 	
 	return currentADCValue;										
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Software/Final Code Working Directory/Complete Project/emg.h
+++ Software/Final Code Working Directory/Complete Project/emg.h
@@ -1,8 +1,11 @@
 #ifndef EMG_H
 #define EMG_H
+
+#include <defines.h>
+
 /*
 Prosthetic Hand Senior Design Project
-9/25/2016
+12/3/2016
 Rachel Mertz
 Matthew Varas
 Donnell Jones
@@ -14,25 +17,41 @@ Donnell Jones
     Header file for EMG related variables and functions 
 */
 
-#include <built_in.h>						//?????  Unsure if this is needed ?????
-#include <defines.h>
-#include <globals.h>
-
-#define AWD_INITIAL_LOW_THRESH		400			//Const  - Lower threshold for analog watchdog in initial state (waiting for low->high transition)
-#define AWD_INITIAL_HIGH_THRESH		3695		//Const  - Upper threshold for analog watchdog in initial state (waiting for low->high transition)
-#define AWD_ARMED_LOW_THRESH		3695		//Const  - Lower threshold for analog watchdog in armed state (waiting for high->low transition)  (same as high threshold in initial state)
-#define AWD_ARMED_HIGH_THRESH		1000000		//Const  - Upper (unreachable) threshold for analog watchdog in armed state (waiting for high->low transition)
 
 
-//Variables
+// Define thresholds mode 0 (Waiting for EMG activation)
+#define high_level        400		// Upper threshold for analog watchdog in initial state (waiting for low->high transition)
+#define low_level         0			// Lower threshold for analog watchdog in initial state (waiting for low->high transition)
+
+// Define thresholds mode 1 (EMG activated, currently timing)
+#define high_level2       4095		// Upper (unreachable) threshold for analog watchdog in armed state (waiting for high->low transition)
+#define low_level2        400		// Lower threshold for analog watchdog in armed state (waiting for high->low transition)  (same as high threshold in initial state)
 
 
 
-/************************  EMG Function Prototypes  ************************/
-void initialize_emg();						//Initialization of analog watchdog and other EMG items 
-void ADC_AWD();								//Analog Watchdog interrupt handler   (???Can we re-name this???)
-void set_initial_awd_window();				//Sets the AWD window to the initial state (waiting for low->high transition)
-void set_armed_awd_window();				//Sets the AWD window to the armed state (waiting for the high->low transition) 
-unsigned long get_current_emg_value();		//Samples the EMG signal and returns the current value 
+/* --------------------------- Function Prototypes ---------------------------- */
+void init_emg();					// Initialize Electromyography
+void AWD_ISR();               		// ADC interrupt handler
+void emg_override_ISR();			// EMG Override button interrupt handler 
+void init_emg_timer();            	// Timer 11 init
+void emg_timer_ISR();      			// Timer 11 interrupt handler
+unsigned long read_emg();			// Samples and returns current EMG value 
+
+
+
+/* --------------------------- Variables ---------------------------- */
+int system_go = 0;			  // Flag to start hand operation 
+int go_status = 0;			  // Current operating status of hand (active or inactive)
+
+
+
+/* --------------------------- Constants ---------------------------- */
+unsigned int EMG_TIMER_PSC = 7874				// Prescaler value for EMG timer 10 (3s)
+unsigned int EMG_TIMER_RELOAD = 63999			// Auto reload value for EMG timer 10 (3s)
+
+
+
+
+
 
 
